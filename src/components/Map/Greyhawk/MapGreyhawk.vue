de1b1cc63615dbe407cd46c6fdb65cae5f34a4c1
Etienne Baudry <webmaestro.fr@gmail.com>
2024-03-18 14:46:41 +0700

    Config PanZoom

--- src/components/Map/Greyhawk/MapGreyhawk.vue
+++ src/components/Map/Greyhawk/MapGreyhawk.vue
@@ -1,16 +1,25 @@
 <template>
-  <img class="map" src="./MapGreyhawk.svg" ref="mapRef" />
+  <div class="map-container" ref="containerRef">
+    <img class="map-image" src="./MapGreyhawk.svg" ref="imageRef" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Panzoom from '@panzoom/panzoom'
 import { ref, onMounted } from 'vue'
 import './MapGreyhawk.css'
-const mapRef = ref<HTMLElement>()
+const containerRef = ref<HTMLDivElement>()
+const imageRef = ref<HTMLImageElement>()
 onMounted(() => {
-  console.log('mapRef', mapRef)
-  if (mapRef.value) {
-    Panzoom(mapRef.value, { animate: false, duration: 0 })
+  if (containerRef.value && imageRef.value) {
+    const panzoom = Panzoom(imageRef.value, {
+      animate: false,
+      duration: 0,
+      maxScale: 10,
+      cursor: 'grab',
+      contain: 'outside'
+    })
+    containerRef.value.addEventListener('wheel', panzoom.zoomWithWheel)
   }
 })
 </script>
