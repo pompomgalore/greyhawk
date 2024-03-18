fefdfbffe5c218a6e0ecb4469f8068f185c88219
Etienne Baudry <webmaestro.fr@gmail.com>
2024-03-18 16:04:22 +0700

    Clean Up SVG

--- src/components/Map/Greyhawk/MapGreyhawk.vue
+++ src/components/Map/Greyhawk/MapGreyhawk.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="map-container" ref="containerRef">
-    <img class="map-image" src="./MapGreyhawk.svg" ref="imageRef" />
+    <map-greyhawk-vector class="map-image" ref="imageRef" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import Panzoom from '@panzoom/panzoom'
 import { ref, onMounted } from 'vue'
+import MapGreyhawkVector from './MapGreyhawkVector.vue'
 import './MapGreyhawk.css'
 const containerRef = ref<HTMLDivElement>()
-const imageRef = ref<HTMLImageElement>()
+const imageRef = ref<SVGSVGElement>()
 onMounted(() => {
   if (containerRef.value && imageRef.value) {
     const panzoom = Panzoom(imageRef.value, {
