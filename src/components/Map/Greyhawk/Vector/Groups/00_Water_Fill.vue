6ae04fa4d9f709f1bf9dc8cad16a85dbbacce53c
Etienne Baudry <webmaestro.fr@gmail.com>
2024-03-21 11:08:41 +0700

    Fix water layers

--- src/components/Map/Greyhawk/Vector/Groups/00_Water_Fill.vue
+++ src/components/Map/Greyhawk/Vector/Groups/00_Water_Fill.vue
@@ -1,9 +1,5 @@
 <template>
-  <g id="00_Water_Fill" style="display: inline" transform="translate(1.5 1.5)">
-    <path
-      id="rect731"
-      d="M0 0h44000.01v34000.01H0z"
-      style="fill: #e4f5f8; stroke: #373435; stroke-width: 3; stroke-miterlimit: 2.61313"
-    />
+  <g id="greyhawk-00_Water_Fill" class="map-water-fill">
+    <rect x="680" y="1280" width="43060" height="32300" />
   </g>
 </template>
