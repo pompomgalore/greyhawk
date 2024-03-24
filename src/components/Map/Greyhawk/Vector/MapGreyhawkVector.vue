bfd790682d25dd3e8daf69c75e3f28a31b622bf9
Etienne Baudry <webmaestro.fr@gmail.com>
2024-03-25 00:34:49 +0700

    Fix Kingdom Names

--- src/components/Map/Greyhawk/Vector/MapGreyhawkVector.vue
+++ src/components/Map/Greyhawk/Vector/MapGreyhawkVector.vue
@@ -21,11 +21,11 @@
     <ForestFill />
     <SnowFill />
 
-    <ForestContours />
+    <!-- <ForestContours />
     <PlainsContours />
     <MountainContours />
     <HillsContours />
-    <FeaturesContours />
+    <FeaturesContours /> -->
 
     <RoadsLine />
     <LakeStroke />
@@ -36,8 +36,8 @@
     <SettlementsIcons />
 
     <TerrainNames />
-    <!-- <KingdomNames />
-    <SettlementNames /> -->
+    <KingdomNames />
+    <!-- <SettlementNames /> -->
     <DarleneSignature />
   </svg>
 </template>
@@ -54,11 +54,11 @@ import MountainFill from './Groups/07_Mountain_Fill.vue'
 import PlainsFill from './Groups/08_Plains_Fill.vue'
 import ForestFill from './Groups/09_Forest_Fill.vue'
 import SnowFill from './Groups/10_Snow_Fill.vue'
-import ForestContours from './Groups/11_Forest_Contours.vue'
-import PlainsContours from './Groups/12_Plains_Contours.vue'
-import MountainContours from './Groups/13_Mountain_Contours.vue'
-import HillsContours from './Groups/14_Hills_Contours.vue'
-import FeaturesContours from './Groups/15_Features_Contours.vue'
+// import ForestContours from './Groups/11_Forest_Contours.vue'
+// import PlainsContours from './Groups/12_Plains_Contours.vue'
+// import MountainContours from './Groups/13_Mountain_Contours.vue'
+// import HillsContours from './Groups/14_Hills_Contours.vue'
+// import FeaturesContours from './Groups/15_Features_Contours.vue'
 import RoadsLine from './Groups/16_Roads_Line.vue'
 import LakeStroke from './Groups/17_Lake_Stroke.vue'
 import CoastStroke from './Groups/18_Coast_Stroke.vue'
@@ -66,7 +66,7 @@ import RiverOutline from './Groups/19_River_Outline.vue'
 import RiverLine from './Groups/20_River_Line.vue'
 import SettlementsIcons from './Groups/21_Settlements_Icons.vue'
 import TerrainNames from './Groups/22_Terrain_Names.vue'
-// import KingdomNames from './Groups/23_Kingdom_Names.vue'
+import KingdomNames from './Groups/23_Kingdom_Names.vue'
 // import SettlementNames from './Groups/24_Settlement_Names.vue'
 import DarleneSignature from './Groups/25_Darlene_Signature.vue'
 
