fbd25f93eb0ce0d6f6808bcb4219ad6451c9b805
Etienne Baudry <etienne@webmaestro.fr>
2024-04-08 08:05:48 +0700

    Update LegendGreyhawkFooter.vue

--- src/components/Legend/Greyhawk/LegendGreyhawkFooter.vue
+++ src/components/Legend/Greyhawk/LegendGreyhawkFooter.vue
@@ -1,12 +1,5 @@
 <template>
   <p>
-    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" class="cc">
-      <img
-        src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-sa.png"
-        width="49"
-        height="17"
-      />
-    </a>
     <a href="https://github.com/pompomgalore" target="_blank">Pom Pom Galli</a> 2024
   </p>
 </template>
@@ -16,11 +9,5 @@ p {
   font-size: small;
   line-height: 1.2;
   margin-top: 2.5rem;
-}
-.cc {
-  display: inline-block;
-  line-height: 0;
-  margin: 0 0.25em 0 0;
-  vertical-align: text-bottom;
 }
 </style>
