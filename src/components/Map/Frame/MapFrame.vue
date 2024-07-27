14219662e0b54638a94ce8ac2cdb57339267f048
Etienne Baudry <webmaestro.fr@gmail.com>
2024-07-28 00:19:11 +0700

    Clamp pan

--- src/components/Map/Frame/MapFrame.vue
+++ src/components/Map/Frame/MapFrame.vue
@@ -1,32 +1,31 @@
 <script lang="ts" setup>
+import { clamp } from 'lodash'
 import { ref, onMounted } from 'vue'
+
+interface MapFrameTransform {
+  x: number
+  y: number
+  scale: number
+}
 
 const { width, height } = defineProps<{
   width: number
   height: number
 }>()
 
-const contentRef = ref<HTMLElement>()
-const frameRef = ref<HTMLElement>()
-const transform = ref<{ scale: number; x: number; y: number }>({ scale: 1, x: 0, y: 0 })
-const previousPointerEvent = ref<PointerEvent>()
+const contentRef = ref<HTMLDivElement>()
+const frameRef = ref<HTMLDivElement>()
+const transform = ref<MapFrameTransform>({ scale: 1, x: 0, y: 0 })
+const pointerPosition = ref<{ x: number; y: number }>({ x: 0, y: 0 })
 
 function queryMapSelector(selector: string) {
   if (contentRef.value && selector) {
     return contentRef.value.querySelector(selector)
   }
 }
-
 function getCenterPoint(element: Element): Point {
   const { top, left, width, height } = element.getBoundingClientRect()
   return [left + width / 2, top + height / 2]
-}
-
-function focusOnPoint([x, y]: Point) {
-  if (contentRef.value && frameRef.value) {
-    const { width: frameWidth, height: frameHeight } = frameRef.value.getBoundingClientRect()
-    transform.value = { x: x - frameWidth / 2, y: y - frameHeight / 2, scale: 1 }
-  }
 }
 
 function focusOnElement(selector: string) {
@@ -36,24 +35,42 @@ function focusOnElement(selector: string) {
     focusOnPoint(point)
   }
 }
-
-function handlePointerPosition(event: PointerEvent) {
-  previousPointerEvent.value = event
+function focusOnPoint([x, y]: Point) {
+  if (frameRef.value) {
+    const { width: frameWidth, height: frameHeight } = frameRef.value.getBoundingClientRect()
+    transform.value = { x: frameWidth / 2 - x, y: frameHeight / 2 - y, scale: 1 }
+  }
 }
+
 function handlePointerMove(event: PointerEvent) {
   if (event.buttons > 0) {
-    if (previousPointerEvent.value) {
-      const { clientX: lastX, clientY: lastY } = previousPointerEvent.value
-      const { clientX, clientY } = event
-      const { x, y, scale } = transform.value
-      transform.value = {
-        x: x - (clientX - lastX) / scale,
-        y: y - (clientY - lastY) / scale,
-        scale
-      }
-    }
-    previousPointerEvent.value = event
+    const { x: previousX, y: previousY } = pointerPosition.value
+    translateBy(event.clientX - previousX, event.clientY - previousY)
+    handlePointerPosition(event)
   }
+}
+function handlePointerPosition(event: PointerEvent) {
+  pointerPosition.value = { x: event.clientX, y: event.clientY }
+}
+
+function translateBy(x: number, y: number) {
+  const { x: currentX, y: currentY, scale } = transform.value
+  transform.value = clampTransform({
+    x: currentX + x / scale,
+    y: currentY + y / scale,
+    scale
+  })
+}
+function clampTransform({ x, y, scale }: MapFrameTransform): MapFrameTransform {
+  if (frameRef.value) {
+    const { width: frameWidth, height: frameHeight } = frameRef.value.getBoundingClientRect()
+    return {
+      x: clamp(x, frameWidth - width, 0),
+      y: clamp(y, frameHeight - height, 0),
+      scale
+    }
+  }
+  return { x, y, scale }
 }
 
 defineExpose({ focusOnElement, focusOnPoint, queryMapSelector })
@@ -77,7 +94,7 @@ onMounted(() => {
       :style="{
         width: `${width}px`,
         height: `${height}px`,
-        transform: `translate(${-transform.x}px, ${-transform.y}px) scale(${transform.scale})`
+        transform: `translate(${transform.x}px, ${transform.y}px) scale(${transform.scale})`
       }"
     >
       <slot :scale="transform.scale" />
